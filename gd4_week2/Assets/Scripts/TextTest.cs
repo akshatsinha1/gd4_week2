7db1ff5e521f77a908adb3c61953788c2da99135
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2024-11-25 15:24:24 +0000

    Initial Commit
    Player Controls

---
+++ gd4_week2/Assets/Scripts/TextTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+
+public class TextTest : MonoBehaviour
+{
+    public TMP_Text text1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            text1.text = "Player Controls Enabled";
+        }
+    }
+}
