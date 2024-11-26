1228500d38c7b24fa97e60db22768ab8aa9dbc82
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2024-11-26 11:14:52 +0000

    Day 2 - Morning
    Player Control Script

---
+++ gd4_week2/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //METHOD 1
+        
+        //transform.position = transform.position + new Vector3(0, 0, movementSpeed * Time.deltaTime);
+        //transform.position += new Vector3(0, 0, movementSpeed * Time.deltaTime);
+
+
+
+        //METHOD 2
+        //transform.Translate(0,0, movementSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        //movementSpeed += Time.deltaTime * 0.2f;
+
+        //METHOD 3
+        rb.AddForce(Vector3.forward * movementSpeed);
+
+    }
+}
